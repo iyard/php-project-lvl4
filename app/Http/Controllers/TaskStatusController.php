755af85f9df82dd2f82a1420675073b8279a66ad
i-yard <iyardsev@gmail.com>
2020-05-17 14:05:05 +0300

    clean code in TaskStatusController

--- app/Http/Controllers/TaskStatusController.php
+++ app/Http/Controllers/TaskStatusController.php
@@ -14,34 +14,18 @@ class TaskStatusController extends Controller
              ->except('index');
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $statuses = TaskStatus::all();
         return view('statuses.index', compact('statuses'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         $status = new TaskStatus();
         return view('statuses.create', compact('status'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -57,24 +41,11 @@ class TaskStatusController extends Controller
             ->route('statuses.index');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit(TaskStatus $status)
     {
         return view('statuses.edit', compact('status'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, TaskStatus $status)
     {
         $this->validate($request, [
@@ -89,12 +60,6 @@ class TaskStatusController extends Controller
             ->route('statuses.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(TaskStatus $status)
     {
         $status->delete();
